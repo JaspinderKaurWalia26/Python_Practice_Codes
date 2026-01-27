256f8ff1d2131e75f015d87c39b33cc3275b667f
Jaspinder Kaur <jaspinderkaurwalia855@gmail.com>
2026-01-27 17:38:55 +0530

    Modified tasks.py

--- 27-01-2026/tasks.py
+++ 27-01-2026/tasks.py
@@ -43,4 +43,43 @@ border[1:-1,1:-1]=0
 #Printing the array with internal values set to zero and border values set to one 
 print(border)
 
+"""
+Task 4: Conditional Filtering Generate an array of 50 random integers between 1 and 100. Replace all odd numbers with -1 without creating a new array (in-place modification).
+"""
 
+#Generating an array
+arr=np.random.randint(1,101,size=50)
+#Replacing odd numbers with -1
+arr[arr%2!=0]=-1
+print(arr)
+
+"""
+Task 5: Finding the Nearest Value Create a 1D array of 20 random floats. Given a target value (e.g., 0.5), find the value in the array that is closest to that target and return its index.
+"""
+
+arr=np.random.random(20)
+print(arr)
+target=0.5
+#Finding the index of closest array
+index=np.abs(arr-target).argmin()
+print("Index=",index)
+
+
+"""
+Task 6: The "Moving Average" Trend FinderMarketing data often has daily spikes. We use a moving average to see the real growth trend.The Task: Create an array representing 15 days of website traffic (e.g., [150, 160, 155, 180, 200...]).The Challenge: Calculate a 3-day moving average. The first result should be the average of days 1, 2, and 3; the second should be the average of days 2, 3, and 4, and so on.Hint: Look into np.cumsum or use clever slicing.
+"""
+
+web_traffic=np.array([150,160,155,180,200,190,220,200,165,170,210,175,185,225,220])
+#Finding average using slicing
+average=(web_traffic[:-2]+web_traffic[1:-1]+web_traffic[2:])/3
+print(average)
+
+"""
+Task 7 : Image "Crop & Flip" Logic
+"""
+
+image=np.random.randint(0,256,(100,100,3))
+cropped=image[25:75,25:75,:]
+flipped=cropped[:,::-1,:]
+print("Cropped Image Shape:",cropped.shape)
+print("Flipped Image Shape:",flipped.shape)
