442d1c3fac972f1bc916b2c01965ff92f0039639
Jaspinder Kaur <jaspinderkaurwalia855@gmail.com>
2026-01-30 10:31:00 +0530

    Modified tasks.py

--- 29-01-2026/tasks.py
+++ 29-01-2026/tasks.py
@@ -7,6 +7,8 @@ Annotate the "Best Day" (the point with the highest revenue) by pointing an arro
 """
 import matplotlib.pyplot as plt
 import random
+import pandas as pd
+import numpy as np
 
 #Generating random values
 ad_spend=[random.randint(100,1000) for _ in range(20)]
@@ -17,16 +19,20 @@ max_revenue=max(revenue)
 best_index=revenue.index(max_revenue)
 # annotating the graph
 plt.annotate("Best Day",xy=(ad_spend[best_index],revenue[best_index]),xytext=(ad_spend[best_index]+50,revenue[best_index]+50),arrowprops=dict(arrowstyle="->"))
+# assigning x-axis label
 plt.xlabel("Ad_Spend")
+# assigning y-axis label
 plt.ylabel("Revenue")
+# assigning title
 plt.title("Ad_Spend vs Revenue")
 plt.legend()
+# plotting grid 
 plt.grid(True)
-plt.savefig("ScatterPlot")
+# showing the plot
 plt.show()
 
 """
-TASK 1: THE CLIENT PROJECT AUDIT
+TASK : THE CLIENT PROJECT AUDIT
 Create a DataFrame from the 'project_data' dictionary below.
 Find and remove any rows where the 'Client_Name' is missing.
 For projects where the 'Budget' is missing, fill it with the AVERAGE budget of the remaining projects.
@@ -39,8 +45,7 @@ project_data = {
     'Service': ['Web Dev', 'App Dev', 'SEO', 'Web Dev', 'Graphic Design']
 }
 """
-import pandas as pd
-import numpy as np
+
 
 project_data = {
     'Project_ID': [101, 102, 103, 104, 105],
@@ -48,6 +53,7 @@ project_data = {
     'Budget': [25000, 15000, None, 30000, 12000],
     'Service': ['Web Dev', 'App Dev', 'SEO', 'Web Dev', 'Graphic Design']
 }
+# Creating dataframe
 df=pd.DataFrame(project_data)
 print(df)
 # removing missing client name
@@ -60,7 +66,7 @@ print(project_budget)
 
 
 """
-TASK 2: MONTHLY AD-SPEND ANALYSIS
+TASK : MONTHLY AD-SPEND ANALYSIS
 Create a DataFrame from 'ad_data'.
 Group the data by 'Platform' and calculate the TOTAL spend for each.
 Create a Bar Chart showing the total spend per platform.
@@ -81,20 +87,28 @@ ad_data = {
     'Platform': ['Google', 'FB Ads', 'Instagram', 'Google', 'FB Ads', 'Instagram'],
     'Spend': [5000, 4000, 2500, 5500, 3800, 2900]
 }
+# Creating dataframe
 df=pd.DataFrame(ad_data)
+# calculating total spend by each platform
 total_spend=df.groupby('Platform')['Spend'].sum()
 print(total_spend)
-import matplotlib.pyplot as plt
+# creating a bar charts 
 total_spend.plot(kind="bar")
 colors=["Red","Green","Blue"]
+# assigning title 
 plt.title("Ad Spend Distribution by Platform")
+# assigning x-axis label
 plt.xlabel("Platform")
+# assigning y-axis label
 plt.ylabel("Spend")
+# plotting grid for y-axis only
 plt.grid(axis="y")
+#showing the bar chart
 plt.show()
 
 
 """
+Task:
 Create a DataFrame from 'seo_stats'.
 Create a new column called 'CTR' (Click-Through Rate).
 Formula: CTR = (Clicks / Impressions) * 100
@@ -116,25 +130,36 @@ seo_stats = {
     'Clicks': [120, 85, 150, 90, 110],
     'Impressions': [2000, 1500, 2500, 1600, 1900]
 }
-
+# Creating dataframe
 df=pd.DataFrame(seo_stats)
 print(df)
+# creating a new column 'CTR'
 df['CTR']=(df['Clicks']/df['Impressions'])*100
 print(df)
+# converting date column to datetime format
 df['Date']=pd.to_datetime(df['Date'])
+# sorting the date
 df=df.sort_values('Date')
 print(df)
+# assigning figure size
 plt.figure(figsize=(8,5))
+# plotting the line chart
 plt.plot([df.Date],[df.Clicks],marker="o")
 plt.xticks(df['Date'])
+# finding the maximum clicks
 max_row=df.loc[df['Clicks'].idxmax()]
+# annotating the line chart
 plt.annotate(
     "Highest Clicks",
     xy=(max_row['Date'],max_row['Clicks']),
     xytext=(max_row['Date'],max_row['Clicks']+10),
     arrowprops=dict(arrowstyle="->")
 )
+# assigning x-axis label
 plt.xlabel('Date')
+# assigning y-axis label
 plt.ylabel('Clicks')
+# plotting grid 
 plt.grid(True)
+# showing line chart
 plt.show()
